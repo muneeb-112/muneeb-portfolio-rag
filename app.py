15a489886f9ba72c065efcb5796914381ad52df9
muneeb-112 <munibullah157@gmail.com>
2026-08-29 23:32:02 +0500

    Updated model to gemini-3.6-flash

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 # 1. Gemini LLM Model Setup
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash", api_key=api_key, temperature=0
+    model="gemini-3.6-flash", api_key=api_key, temperature=0
 )
 
 # 2. Page Settings
